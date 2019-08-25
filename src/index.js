ad6d9f829cbbac0482c6d07c198c53b3c4427a23
Desmond Edem <edemintech@gmail.com>
2019-08-26 00:13:23 +0100

    ch(project setup): setup project for development

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import debug from 'debug';
+import express from 'express';
+// import swaggerUi from 'swagger-ui-express';
+import eventRoute from './routes/events.routes';
+
+const app = express();
+const DEBUG = debug('dev');
+const PORT = process.env.PORT || 4500;
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/v1', eventRoute);
+// app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Welcome to Events',
+  });
+});
+
+app.get('*', (req, res) => {
+  res.status(404).json({
+    status: 'error',
+    error: 'Resource does not exist',
+  });
+});
+
+app.listen(PORT, () => DEBUG(`Server running on port ${PORT}`));
+
+export default app;
---
+++ src/routes/events.routes.js
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const eventRoute = Router();
+
+export default eventRoute;
